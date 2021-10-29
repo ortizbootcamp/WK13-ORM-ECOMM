7d3d001580b483bed845c4e02bc2e47405a2abf4
Natasha Ortiz <tashaaortiz@gmail.com>
2021-10-28 23:35:10 -0500

    TS route errors

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,9 +4,8 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
   Product.findAll({
     attributes: ["id", "product_name", "price", "stock", "category_id"],
     include: [
@@ -21,8 +20,8 @@ router.get('/', (req, res) => {
       },
     ],
   })
-    .then((dbProductData) => res.json(dbProductData))
-    .catch((err) => {
+    .then(dbProductData => res.json(dbProductData))
+    .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
